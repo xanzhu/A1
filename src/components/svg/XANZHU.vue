26cd70f5817f0b5be8f3565105ec8b63757784aa
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-05-02 21:49:10 +1200

    Minor updates * Cleaned up formatting on files * Added a 404 page (Testing) * Increased STS age * Added back SSL settings previously used.

---
+++ src/components/svg/XANZHU.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg viewBox="0 0 348 107.04" xmlns="http://www.w3.org/2000/svg"><text transform="translate(5.08 68.56)" font-size="80" font-family="HelveticaNeue-Bold, Helvetica Neue" font-weight="700">XANZHU</text></svg>
+</template>
